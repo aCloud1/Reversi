e7cb244bb2bfbcc37b3bdedde37331735e7db19a
Almond <almondCloud@outlook.com>
2023-10-29 19:32:15 +0200

    Colors

--- src/main/java/com/example/reversi/Renderer.java
+++ src/main/java/com/example/reversi/Renderer.java
@@ -8,6 +8,9 @@ public class Renderer {
     int CELL_WIDTH, CELL_HEIGHT;
     GamePanel game;
 
+    private float ghost_opacity = 0.3f;
+    private Color board_gray1, board_gray2;
+
     Hashtable<Integer, Color> player_to_color, player_to_ghost_color;
     public Renderer(int cell_width, int cell_height, GamePanel game) {
         this.CELL_WIDTH = cell_width;
@@ -19,8 +22,11 @@ public class Renderer {
         player_to_color.put(Cell.PLAYER2.getValue(), new Color(0.0f, 0.0f, 1.0f, 1.0f));
 
         player_to_ghost_color = new Hashtable<>();
-        player_to_ghost_color.put(Cell.PLAYER1.getValue(), new Color(1.0f, 0.0f, 0.0f, 0.15f));
-        player_to_ghost_color.put(Cell.PLAYER2.getValue(), new Color(0.0f, 0.0f, 1.0f, 0.15f));
+        player_to_ghost_color.put(Cell.PLAYER1.getValue(), new Color(1.0f, 0.0f, 0.0f, ghost_opacity));
+        player_to_ghost_color.put(Cell.PLAYER2.getValue(), new Color(0.0f, 0.0f, 1.0f, ghost_opacity));
+
+        board_gray1 = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+        board_gray2 = new Color(0.35f, 0.35f, 0.35f, 1.0f);
     }
 
     public void drawBoard(Graphics g, Board board, int player_color) {
@@ -31,8 +37,8 @@ public class Renderer {
             for(int y = 0; y < board.COL_COUNT; y++)
             {
                 // draw board
-                if((x+y) % 2 == 0)  g.setColor(Color.GRAY);
-                else                g.setColor(Color.DARK_GRAY);
+                if((x+y) % 2 == 0)  g.setColor(board_gray1);
+                else                g.setColor(board_gray2);
                 g.fillRect(x * CELL_WIDTH, y * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT);
 
                 if(valid_moves.getCell(x, y) == 9)
